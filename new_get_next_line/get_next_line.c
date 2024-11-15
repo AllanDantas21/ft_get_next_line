8d8ba56a29434dfcf870f9a1e76b8e479a4e2ad8
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2024-11-15 16:36:31 -0300

    fixs

--- new_get_next_line/get_next_line.c
+++ new_get_next_line/get_next_line.c
@@ -23,38 +23,60 @@ static int ft_check_new_line(const char *s)
 	return (0);
 }
 
+static char *ft_strdup(const char *s)
+{
+    char *str;
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    str = malloc(sizeof(char) * (i + 1));
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (s[i])
+    {
+        str[i] = s[i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
 static char *get_line(t_data *data)
 {
     char *buff;
     int ret;
     t_list *node;
 
-    buff = malloc(sizeof(char) * BUFFER_SIZE);
-    if (!buff)
-      return (NULL);
+    buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
     while (42)
     {
         ret = read(data->fd, buff, BUFFER_SIZE);
         if (ret <= 0)
             break;
         buff[ret] = '\0';
-        add_node_in_list(data, buff); 
+        add_node_in_list(data, ft_strdup(buff));
         if (ft_check_new_line(buff))
-          break;
+            break;
     }
     return (NULL);
 }
 
 char *get_next_line(int fd)
 {
-    static char *backup_line = NULL;
+    //static char *backup_line = NULL;
     t_data data;
 
     data = (t_data){0};
     data.fd = fd;
     data.head = NULL;
-    backup_line = get_line(&data);
+    // if (backup_line)
+    //     add_node_in_list(&data, backup_line);
+    get_line(&data);
     extract_ret_line(&data);
+   //  backup_line = extract_backup(&data);
     free_all_list(&data);
     return (data.ret_line);
 }
@@ -73,6 +95,12 @@ int main(void)
         perror("Erro ao abrir o arquivo");
         return 1;
     }
+    line = get_next_line(fd);
+    printf("%s", line);
+    free(line);
+    line = get_next_line(fd);
+    printf("%s", line);
+    free(line);
     line = get_next_line(fd);
     printf("%s", line);
     free(line);
--- new_get_next_line/get_next_line_utils.c
+++ new_get_next_line/get_next_line_utils.c
@@ -11,49 +11,90 @@
 #include "get_next_line.h"
 #include "stdio.h"
 
+void copy_lines(t_list *node, char *dest, int start_from_newline)
+{
+    int i = 0;
+    int j = 0;
+    int found_newline = 0;
+
+    while (node)
+    {
+        j = 0;
+        while (node->line[j])
+        {
+            if (start_from_newline && found_newline)
+                dest[i++] = node->line[j];
+            else if (!start_from_newline)
+                dest[i++] = node->line[j];
+            if (node->line[j] == '\n')
+                found_newline = 1;
+            j++;
+        }
+        node = node->next;
+    }
+    dest[i] = '\0';
+}
+
 void extract_ret_line(t_data *data)
 {
     t_list *node;
     char *line;
     int len;
     int i;
-    int j;
 
+    if (!data->head)
+        return;
     node = data->head;
     len = 0;
     while (node)
     {
         i = 0;
-        while (node->line[i] && node->line[i] != '\n')
+        while (node->line[i])
         {
             len++;
+            if (node->line[i] == '\n')
+                break;
             i++;
         }
         node = node->next;
     }
     line = malloc(len + 1);
     if (!line)
-        return ;
-    node = data->head;
-    i = 0;
-    while (node)
-    {
-        j = 0;
-        while (node->line[j] && node->line[j] != '\n')
-        {
-            line[i++] = node->line[j++];
-        }
-        node = node->next;
-    }
-    line[i] = '\0';
-    printf("%s", line);
+        return;
+    copy_lines(data->head, line, 0);
     data->ret_line = line;
 }
 
-/*char *extract_backup()
+char *extract_backup(t_data *data)
 {
-  // extrair o backup
-}*/
+    t_list *node;
+    char *backup;
+    int len;
+    int i;
+    int found_newline;
+
+    node = data->head;
+    len = 0;
+    found_newline = 0;
+    while (node)
+    {
+        i = 0;
+        while (node->line[i])
+        {
+            if (found_newline)
+                len++;
+            if (node->line[i] == '\n')
+                found_newline = 1;
+            i++;
+        }
+        node = node->next;
+    }
+    backup = malloc(len + 1);
+    if (!backup)
+        return NULL;
+    copy_lines(data->head, backup, 1);
+    return backup;
+}
 
 void free_all_list(t_data *data)
 {
@@ -76,7 +117,6 @@ void add_node_in_list(t_data *data, char *line)
     node = malloc(sizeof(t_list));
     if (!node)
         return;
-    //printf("line dentro da add node: %s", line);
     node->line = line;
     node->next = NULL;
     if (!data->head)
@@ -89,4 +129,3 @@ void add_node_in_list(t_data *data, char *line)
         current = current->next;
     current->next = node;
 }
-
